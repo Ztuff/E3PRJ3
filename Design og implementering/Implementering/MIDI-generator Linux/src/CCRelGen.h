4d74ca08e79adda6dacb96ef480be6c87f3e5bf6
Lukas Hedegaard <lukas_hedegaard@hotmail.com>
2014-10-23 13:44:14 +0200

    ..

---
+++ Design og implementering/Implementering/MIDI-generator Linux/src/CCRelGen.h
@@ -0,0 +1,21 @@
+#ifndef CCRELGEN_H_
+#define CCRELGEN_H_
+
+#include "MIDIGen.h"
+
+using namespace std;
+
+class CCRelGen : public MIDIGen
+{
+public:
+	CCRelGen(string speed = "medium", int dataIn = 0);		
+	virtual void sendMIDI(int dataIn = -1);		//dataIn is optional	
+	virtual void printStrategies();
+private:
+	/**** dataIn is Rel CC change with 0-point at 64 ****/
+	string speed_;								//strategies: "slow", "medium", "fast"
+	int speedScaling_;							//utility variable
+	int dataInOld_;								//utility variable - used in playContNote
+};
+
+#endif /* CCRELGEN_H_ */
