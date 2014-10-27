d721e144dc77ad4e6371bffbe9e3b0a9a83e9362
mathiassiig <mathias_sn@hotmail.com>
2014-10-27 13:16:18 +0100

    Rough trådeopsætning i main

--- Design og implementering/Implementering/Rock Main/main-udkast.cpp
+++ Design og implementering/Implementering/Rock Main/main-udkast.cpp
@@ -1,24 +1,43 @@
 //#include "MIDIGen.h"
-
+#include <pthread.h>
 int main()
 {
-	/*
-	- Opret en tråd (sensToMidi) for hver sensorKonfiguration.
-	SensorData:
-		1: Main får en struct af 'Bluetooth Rock'.
-		2: Hvilken sensor er det fra? Tjekkes ved sensorID.
-		3: Sendes til sensToMIDI.
-	- Opret en tråd for QT GUI-modulet.
-	- Opret en tråd for BluetoothRock
-	- Nødvendigt med tråd for sampleLibrary? Don't think so.
-	*/
+	//- Opret en tråd (sensToMidi) for hver sensorKonfiguration.
+	amountOfSensors = 16 //Denne værdi skal assignes fra et preset
+	pthread_t sensorThread[amountOfSensors];
+	for (int i = 0; i < amountOfSensors; i++)
+	{
+		pthread_create(&sensorThread[i], NULL, sensToMidi);//, sidste argument?? struct? preset?);
+	}
 	
+	//SensorData:
+	//	1: Main får en struct af 'Bluetooth Rock'.
+	//	2: Hvilken sensor er det fra? Tjekkes ved sensorID.
+	//	3: Sendes til sensToMIDI.
+	
+	//- Opret en tråd for QT GUI-modulet.
+	
+	//- Opret en tråd for BluetoothRock
+	pthread_t BTRockThread;
+	pthread_create(&BTRockThread, NULL, BluetoothRock); // Argument?
+	//- Nødvendigt med tråd for sampleLibrary? Don't think so.
+	
+	//Join tråde
+	pthread_join(BTRockThread, NULL, BluetoothRock); //Argument?
+	for (int i = 0; i < amountOfSensors; i++)
+	{
+		pthread_join(sensorThread[i], NULL, sensToMidi);//, sidste argument);
+	}
+	return 0;
 }
 
-MIDIOut sensToMIDI(struct sensorStruct sStruct)
+void sensToMIDI(struct sensorStruct sStruct)
 {
 	//En hver sensorKonfiguration har tilknyttet en MIDIgen
 	//MIDIGeneratoren outputter MIDI til Linuxsampleren
+	//Tænker at denne metode køres og kalder Linuxsampleren til sidst.
+	//Fx:
+	
 }
 
 void BluetoothRock()
@@ -32,3 +51,5 @@ void sampleLibrary()
 	//Står for at sende et LsSample videre til Linux Sampleren. 
 	//Linux Sampleren forbinder MIDI-out med sample der giver et lyd output.
 }
+
+//Noget med GUI?
