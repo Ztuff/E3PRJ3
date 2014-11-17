519ec54e86550eb544e3e89c3866713ced156c71
Kristoffer Lerbæk Pedersen <201370952@iha.dk>
2014-11-17 20:47:03 +0100

    Tilrettet baud rate fra 9600 til 115200

--- Design og implementering/Bluetooth/Rock/UART.c
+++ Design og implementering/Bluetooth/Rock/UART.c
@@ -42,7 +42,7 @@ int main()
 	//  PARODD - Odd parity (else even)
 	struct termios options;
 	tcgetattr(uart0_filestream, &options);
-	options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;    //<Set baud rate
+	options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;    //<Set baud rate
 	options.c_iflag = IGNPAR;
 	options.c_oflag = 0;
 	options.c_lflag = 0;
