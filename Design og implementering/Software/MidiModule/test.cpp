6469c422ca729ea12476b5697a242ffe5813e489
Lukas Hedegaard <lukas_hedegaard@hotmail.com>
2014-11-28 13:03:37 +0100

    videre arbejde med test til MidiModule

--- Design og implementering/Software/MidiModule/test.cpp
+++ Design og implementering/Software/MidiModule/test.cpp
@@ -11,7 +11,7 @@ int main()
 {
 	/****************************************** INITIALIZATION **********************************************/
 	/*** Opret DataStruct array Dummy ***/
-	int size = 16;
+	const int size = 16;
 	DataStruct myData[size];
 	
 	for (int i = 0; i< size; ++i)
@@ -24,31 +24,37 @@ int main()
 	/*** Klargør vector<MidiSignal> ***/
 	vector<MidiSignal> midiVec;
 	
+	/*** Opret Mapping Scheme og SensorConfiguration Dummy ***/
+	MappingScheme *testMSchemePtr = new MappingScheme(	"test",						//id
+														"key",						//param
+														"cis", "major", "rising",	//key params
+														NULL,						//velocity params
+														NULL,						//Common CC param
+														NULL, NULL,					//CCAbs params
+														NULL);						//CCRel params
+	
+	
+	SensorConfiguration *testSensConfPtr = new SensorConfiguration("testConfig",	//string name,
+																	0,				//int sensorID,
+																	x,				//char axis,
+																	*testMScheme,	//MappingScheme mScheme,
+																	midiVec);		//vector<MidiSignal> &midiVec);
+	
+	list <SensorConfiguration> myList(*testSensConfPtr);
+	
 	/*** Klargør ALSA-delen ***/
 	//????
 	AlsaAdapter alsaAdapter(3);
 	
-	/*** Opret SensorConfiguration Dummy ***/
-	MappingScheme *testMScheme = new MappingScheme(	"test",						//id
-													"key",						//param
-													"cis", "major", "rising",	//key params
-													NULL,						//velocity params
-													NULL,						//Common CC param
-													NULL, NULL,					//CCAbs params
-													NULL);						//CCRel params
-	
-	
-	SensorConfiguration( 	"testConfig",	//string name,
-							0,				//int sensorID,
-							x,				//char axis,
-							*testMScheme,	//MappingScheme mScheme,
-							midiVec);		//vector<MidiSignal> &midiVec);
-	
 	/*** Klargør MidiModule ***/
-	MidiModule(&alsaAdapter);
+	MidiModule(&alsaAdapter, myList);
 	
 	/******************************************* FUNCITON TEST ***********************************************/
 	
+	MidiModule.run();
+	
+	delete testMSchemePtr;
+	delete testSensConfPtr;
 	
 	return 0;
 }
