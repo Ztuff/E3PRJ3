f0108481c3ae389f19a74cdd44fe127faef99f74
Lukas Hedegaard <lukas_hedegaard@hotmail.com>
2014-12-11 11:34:23 +0100

    Mapping Scheme and MidiSignal test

--- Design og implementering/Software/DataStorage/MappingScheme/MappingScheme/test.cpp
+++ Design og implementering/Software/DataStorage/MappingScheme/MappingScheme/test.cpp
@@ -1,11 +1,13 @@
 #include <vector>
 #include "MappingScheme.h"
-#define VECTORSIZE 16
+
+using namespace std;
 
 int main()
 {
-	MidiSignal mySig;
+	/*MidiSignal mySig;
 	mySig.print();
+
 	mySig.channel_ = '0';
 	mySig.command_ = '8';
 	mySig.param1_ = 0;
@@ -13,27 +15,28 @@ int main()
 
 	mySig.print();
 
-	return 0;
-	/*vector<MidiSignal> MidiSigs(VECTORSIZE);
+	*/
+	
+	int vectorsize = 16;
+	vector<MidiSignal> MidiSigs(vectorsize);
 
-	for (int i = 0; i < VECTORSIZE; ++i)
+	for (int i = 0; i < vectorsize; ++i)
 	{
-		MidiSigs[i].channel_=0;
-		MidiSigs[i].command_=0x80;
+		MidiSigs[i].channel_='0';
+		MidiSigs[i].command_='8';
 		MidiSigs[i].param1_=0;
 		MidiSigs[i].param2_=0;
-		MidiSigs[i].param1Old_=0;
-		MidiSigs[i].param2Old_=0;
 	}
 
-	MidiSigs[16].print();
-	*/
-	/*
-	MidiSignal testSignal = {'1','8',0,0};
-	cout <<   "Original signal\n  TestSignal = " << endl; 
-	testSignal.print();
+	MidiSigs[1].print();
+	
+	cout << "\n\n\n";
+	
+	cout <<   "Original signal\n  MidiSigs[1] = " << endl; 
+	MidiSigs[1].print();
 	
 	MappingScheme *velocityPtr = new MappingScheme(	"one",				//id
+													1,					//kanal
 													"velocity",			//param
 													"0", "0", "0",		//key params
 													20,					//velocity params
@@ -41,56 +44,61 @@ int main()
 													NULL, NULL,			//CCAbs params
 													NULL);				//CCRel params
 
-	velocityPtr->map(50, testSignal);	
-	cout <<   "\nTesting velocity\n  TestSignal = " << endl; 
-	testSignal.print();
+	velocityPtr->map(50, MidiSigs[1]);	
+	cout <<   "\nTesting velocity\n  MidiSigs[1] = " << endl; 
+	MidiSigs[1].print();
 
 	MappingScheme *keyPtr = new MappingScheme(	"two",						//id
+												2,					//kanal
 												"key",						//param
 												"cis", "major", "falling",	//key params
 												NULL,						//velocity params
 												NULL,						//Common CC param
 												NULL, NULL,					//CCAbs params
 												NULL);						//CCRel params
-	keyPtr->map(100, testSignal);
+	keyPtr->map(100, MidiSigs[1]);
 
-	cout <<   "\nTesting key\n  TestSignal = " << endl; 
-	testSignal.print();
+	cout <<   "\nTesting key\n  MidiSigs[1] = " << endl; 
+	MidiSigs[1].print();
 
-	velocityPtr->map(50, testSignal);
-	keyPtr->map(100, testSignal);
+	velocityPtr->map(50, MidiSigs[1]);
+	keyPtr->map(100, MidiSigs[1]);
 
 	cout <<   "\nTesting velocity and (same)key again\n  TestSignal = " << endl; 
-	testSignal.print();
+	MidiSigs[1].print();
 
 	MappingScheme *CCAbsPtr = new MappingScheme("three",		//id
+												3,					//kanal
 												"CCAbs",		//param
 												"0", "0", "0",	//key params
 												NULL,			//velocity params
 												10,				//Common CC param: CC num
 												40, 120,		//CCAbs params
 												NULL);			//CCRel params
-	CCAbsPtr->map(66, testSignal);
+	CCAbsPtr->map(66, MidiSigs[1]);
 	cout <<   "\nTesting CCAbs\n  TestSignal = " << endl; 
-	testSignal.print();
+	MidiSigs[1].print();
 
 	MappingScheme *CCRelPtr = new MappingScheme("four",			//id
+												4,					//kanal
 												"CCRel",		//param
 												"0", "0", "0",	//key params
 												NULL,			//velocity params
 												10,				//Common CC param: CC num
 												40, 120,		//CCAbs params
 												MEDIUM);		//CCRel params
-	CCRelPtr->map(127, testSignal);
+	CCRelPtr->map(127, MidiSigs[1]);
 	cout <<   "\nTesting CCRel\n  TestSignal = " << endl; 
-	testSignal.print();
+	MidiSigs[1].print();
 
 	delete CCRelPtr;
 	delete CCAbsPtr;
 	delete velocityPtr;
 	delete keyPtr; 
 	return 0;
-	*/
+	
+
+	return 0;
 }
 
 
