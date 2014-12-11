d95b36baa4c05fc66bd989e5368f0335c906ffa6
Lukas Hedegaard <lukas_hedegaard@hotmail.com>
2014-12-11 11:37:53 +0100

    MidiSignal rettelse af command og channel til int

--- Design og implementering/Software/MidiModule/DataBank/MidiSignal.h
+++ Design og implementering/Software/MidiModule/DataBank/MidiSignal.h
@@ -1,32 +1,32 @@
 #ifndef MIDISIGNAL_H_
 #define MIDISIGNAL_H_
 
-#define NOTEOFF '8'
-#define NOTEON '9'
-#define AFTERTOUCH 'A'
-#define CONTIUOUSCONTROLLER 'B'
+#define NOTEOFF 0x8
+#define NOTEON 0x9
+#define AFTERTOUCH 0xA
+#define CONTIUOUSCONTROLLER 0xB
 
 #include <string>
 #include <iostream>
 
 struct MidiSignal
 {
-	MidiSignal():	channel_('1'), 
-					command_ ('8'),
+	MidiSignal():	channel_(1), 
+					command_ (8),
 					param1_(0),
 					param2_(0)
 	{}
 
 	/** ATRIBUTES USED FOR ALSA **/
-	char channel_;
-	char command_;
+	int channel_;
+	int command_;
 	int param1_;
 	int param2_;
 
 	void print()
 	{
-		std::cout<<  "     channel_ = " << std::hex << channel_
-				<< "\n     command_ = " << std::hex << command_
+		std::cout<<  "     channel_ = " << std::dec << channel_
+				<< "\n     command_ = " << std::dec << command_
 				<< "\n     param1_ = "<< std::dec << param1_
 				<< "\n     param2_ = "<< std::dec << param2_ << std::endl;
 	}
