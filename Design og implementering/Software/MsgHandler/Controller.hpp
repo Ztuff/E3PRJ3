9d3529601242fb9e872136c9119d25027d9c11e5
Kristoffer Lerbæk Pedersen <201370952@iha.dk>
2014-12-12 13:09:46 +0100

    Filer til MsgHandler

--- Design og implementering/Software/MsgHandler/Controller.hpp
+++ Design og implementering/Software/MsgHandler/Controller.hpp
@@ -1,7 +1,8 @@
 #include <string>
 #include <pthread.h>
 #include "message.hpp"
-
+#include "DataBank.hpp"
+#include "MsgHandlerDefs.hpp"
 
 class Controller
 {
@@ -15,34 +16,15 @@ class Controller
 		void join();
 		
 		void eventDispatcher();
-		
-		struct QtMsg : Message
-		{
-			QtMsg(MsgQueue* msgQPtr, int intArg = -1, 
-				std::string stringArg = NULL)
-			: qtMsgQPtr(msgQPtr),
-			  intArg(intArg),
-			  stringArg(stringArg)				
-			{}
-			MsgQueue* qtMsgQPtr;
-			int intArg;
-			std::string stringArg;
-		};
-		
-		enum typeId
-		{
-			SHUTDOWN_MSG,
-			GET_NEW_SENS_CONF_INFO
-			
-			//put msg types here as they are implemented.
-		};
 			
 	private:		
 		pthread_t threadHandle;
 		
-		MsgQueue msgQ;
+    DataBank dataBank;
+		MsgQueue msgQ( 20 );
 
-		void handleGetNewSensConfInfo(QtMsg* msg);
+		void handleGetSensorKonfInfo( HandlerQueue* msg );
+    void handleSetSensorKonf( SensorKonf* msg );
 		void handleShutdownMsg();
 		void BR3K_error(int errorNum, std::string msg);
 };
